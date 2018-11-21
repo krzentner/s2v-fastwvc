4331db1318b1261d7ce33303ec91a52a7338527c
K.R. Zentner <krzentner@gmail.com>
2018-11-20 22:10:43 -0800

    Don't use actions to control adding nodes.
    This gives all actions essentially the same interpretation, and fixes
    the problem of what reward to use for the add actions.

--- src/mvc_lib/src/lib/mvc_env.cpp
+++ src/mvc_lib/src/lib/mvc_env.cpp
@@ -28,6 +28,7 @@ double MvcEnv::step(int a)
 
 double MvcEnv::stepInner(int a)
 {
+    // TODO: Fix this by recording the best state in mvc_lib.cpp
     // We never record the best solution so far. It's not completely clear how
     // a "best solution so far" fits into the s2v framework.
     // In greedy algorithms, there's just a current state, which is always an
@@ -61,20 +62,36 @@ double MvcEnv::stepInner(int a)
       if (graph->v_in_c[update_v] == 1 && graph->tabu_list[remove_v] != 1)
       {
         Remove(*graph, remove_v);
-        next_action_type = ActionTypeAddV;
-
         graph->time_stamp[remove_v] = current_step;
 
         graph->tabu_list.clear();
         graph->tabu_list.resize(graph->v_num, 0);
 
-        return 0.0;
+        // TODO(krzentner): Investigate using ActionTypeAddV
+        //next_action_type = ActionTypeAddV;
+
+        next_action_type = ActionTypeUpdateTarget;
+
+        while (graph->uncov_stack.size() > 0)
+        {
+            int add_v = ChooseAddV(*graph, remove_v, update_v);
+            Add(*graph, add_v);
+            UpdateEdgeWeight(*graph);
+            graph->tabu_list[add_v] = 1;
+            graph->time_stamp[add_v] = current_step;
+        }
+        RemoveRedundant(*graph);
+        update_v = 0;
+
+        return getReward();
       }
       else
       {
         return repeat_action_reward;
       }
     } else if (next_action_type == ActionTypeAddV) {
+      // TODO(krzentner): Make this code path actually active.
+      assert(false);
       int add_v = a;
       if (graph->v_in_c[add_v] == 1) {
         return invalid_action_reward;
